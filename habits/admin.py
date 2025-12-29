692582ffa20bdab0900b62a38209cba014142331
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-12-29 19:13:18 +0500

    Set up validators

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "action",
+        "time",
+        "place",
+        "is_pleasant",
+        "related_habit",
+        "is_public",
+    )
---
+++ habits/validators.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ValidationError
+
+
+class DurationValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        duration = value.get(self.field)
+        if duration > 120:
+            raise ValidationError("Время выполнения не должно превышать 120 секунд.")
+
+
+class PeriodicityValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        periodicity = value.get(self.field)
+        if periodicity < 1 or periodicity > 7:
+            raise ValidationError("Периодичность должна быть от 1 до 7 дней.")

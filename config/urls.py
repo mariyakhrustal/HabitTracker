9b852f4f6d3e35f0e3b876fc48ba1cd1c958d284
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-12-29 23:42:08 +0500

    Add CRUD for habits, set up permissions, add public habits list

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
-    # path("habits/", include("habits.urls", namespace="habits")),
+    path("", include("habits.urls", namespace="habits")),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitListPagination
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
+
+
+class HabitViewSet(ModelViewSet):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitListPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["update", "destroy", "retrieve"]:
+            permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitListPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)

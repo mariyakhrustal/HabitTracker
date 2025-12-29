f7a64af9b9c1cb01a6bf0191e3febb5ef20c4fc4
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-12-29 19:22:59 +0500

    Add pagination

--- users/permissions.py
+++ users/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.owner == request.user:
+        if obj.user == request.user:
             return True
         return False
